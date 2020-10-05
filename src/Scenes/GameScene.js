8603041add8ea54669205901128781a83602cd74
somoye123 <somoye.ayotunde@gmail.com>
2020-10-05 21:13:07 +0100

    update image path in preload function

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -7,7 +7,8 @@ export default class GameScene extends Phaser.Scene {
  
   preload () {
     // load images
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image('logo', '../src/assets/logo.png');
+
   }
  
   create () {
