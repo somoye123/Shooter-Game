72d8b222c29e74269abd4073c59a9b92e683e007
somoye123 <somoye.ayotunde@gmail.com>
2020-10-06 14:48:29 +0100

    load more assets files into preload and create functions

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef, no-plusplus,radix,import/extensions */
-import 'phaser';
+import "phaser";
 
 export default class PreloaderScene extends Phaser.Scene {
   constructor() {
-    super('Preloader');
+    super("Preloader");
   }
 
   init() {
@@ -11,8 +11,8 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload() {
-    this.add.image(750, 400, 'logo');
-    this.add.image(400, 300, 'background');
+    this.add.image(750, 400, "logo");
+    this.add.image(400, 300, "background");
 
     const progressBar = this.add.graphics();
     const progressBox = this.add.graphics();
@@ -24,10 +24,10 @@ export default class PreloaderScene extends Phaser.Scene {
     const loadingText = this.make.text({
       x: width / 2,
       y: height / 2 - 50,
-      text: 'Loading...',
+      text: "Loading...",
       style: {
-        font: '20px monospace',
-        fill: '#ffffff',
+        font: "20px monospace",
+        fill: "#ffffff",
       },
     });
     loadingText.setOrigin(0.5, 0.5);
@@ -35,10 +35,10 @@ export default class PreloaderScene extends Phaser.Scene {
     const percentText = this.make.text({
       x: width / 2,
       y: height / 2 - 5,
-      text: '0%',
+      text: "0%",
       style: {
-        font: '18px monospace',
-        fill: '#ffffff',
+        font: "18px monospace",
+        fill: "#ffffff",
       },
     });
     percentText.setOrigin(0.5, 0.5);
@@ -46,67 +46,89 @@ export default class PreloaderScene extends Phaser.Scene {
     const assetText = this.make.text({
       x: width / 2,
       y: height / 2 + 50,
-      text: '',
+      text: "",
       style: {
-        font: '18px monospace',
-        fill: '#ffffff',
+        font: "18px monospace",
+        fill: "#ffffff",
       },
     });
     assetText.setOrigin(0.5, 0.5);
 
-    this.load.on('progress', (value) => {
+    this.load.on("progress", (value) => {
       percentText.setText(`${parseInt(value * 100)}%`);
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
       progressBar.fillRect(250, 280, 300 * value, 30);
     });
 
-    this.load.on('fileprogress', (file) => {
+    this.load.on("fileprogress", (file) => {
       assetText.setText(`Loading asset: ${file.key}`);
     });
 
-    this.load.on(
-      'complete',
-      () => {
-        progressBar.destroy();
-        progressBox.destroy();
-        loadingText.destroy();
-        percentText.destroy();
-        assetText.destroy();
-        this.ready();
-      },
-    );
+    this.load.on("complete", () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+      this.ready();
+    });
 
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
-    this.load.image('blueButton1', '../src/assets/ui/blue_button02.png');
-    this.load.image('blueButton2', '../src/assets/ui/blue_button03.png');
-    this.load.image('phaserLogo', '../src/assets/logo.png');
-    this.load.image('box', '../src/assets/ui/grey_box.png');
-    this.load.image('checkedBox', '../src/assets/ui/blue_boxCheckmark.png');
-    this.load.image('platform', '../src/assets/ground.png');
-    this.load.image('background', '../src/assets/forest-bg.png');
-    this.load.image('game-over', '../src/assets/game-over.png');
-    this.load.spritesheet('player', '../src/assets/player.png', {
+    this.load.image("sprBg0", "../src/assets/sprBg0.png");
+    this.load.image("sprBg1", "../src/assets/sprBg1.png");
+    this.load.spritesheet("sprExplosion", "../src/assets/sprExplosion.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+    this.load.spritesheet("sprEnemy0", "../src/assets/sprEnemy0.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.image("sprEnemy1", "../src/assets/sprEnemy1.png");
+    this.load.spritesheet("sprEnemy2", "../src/assets/sprEnemy2.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.image("sprLaserEnemy0", "../src/assets/sprLaserEnemy0.png");
+    this.load.image("sprLaserPlayer", "../src/assets/sprLaserPlayer.png");
+    this.load.spritesheet("sprPlayer", "../src/assets/sprPlayer.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.audio("sndExplode0", "../src/assets/sndExplode0.wav");
+    this.load.audio("sndExplode1", "../src/assets/sndExplode1.wav");
+    this.load.audio("sndLaser", "../src/assets/sndLaser.wav");
+
+    this.load.image("blueButton1", "../src/assets/ui/blue_button02.png");
+    this.load.image("blueButton2", "../src/assets/ui/blue_button03.png");
+    this.load.image("phaserLogo", "../src/assets/logo.png");
+    this.load.image("box", "../src/assets/ui/grey_box.png");
+    this.load.image("checkedBox", "../src/assets/ui/blue_boxCheckmark.png");
+    this.load.image("platform", "../src/assets/ground.png");
+    this.load.image("background", "../src/assets/forest-bg.png");
+    this.load.image("game-over", "../src/assets/game-over.png");
+    this.load.spritesheet("player", "../src/assets/player.png", {
       frameWidth: 24,
       frameHeight: 48,
     });
-    this.load.spritesheet('coin', '../src/assets/star.png', {
+    this.load.spritesheet("coin", "../src/assets/star.png", {
       frameWidth: 20,
       frameHeight: 20,
     });
-    this.load.spritesheet('fire', '../src/assets/fire.png', {
+    this.load.spritesheet("fire", "../src/assets/fire.png", {
       frameWidth: 40,
       frameHeight: 70,
     });
-    this.load.audio('bgMusic', ['../src/assets/TownTheme.mp3']);
+    this.load.audio("bgMusic", ["../src/assets/TownTheme.mp3"]);
   }
 
   ready() {
-    this.scene.start('Title');
+    this.scene.start("Title");
     this.readyCount++;
     if (this.readyCount === 2) {
-      this.scene.start('Title');
+      this.scene.start("Title");
     }
   }
 }
