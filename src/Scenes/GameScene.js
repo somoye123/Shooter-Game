1a2361db012a800d3cd04f9f6cb9c0fce8a588da
somoye123 <somoye.ayotunde@gmail.com>
2020-10-06 14:47:27 +0100

    load assets files into preloader and create functions

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,17 +1,76 @@
-import 'phaser';
- 
+import "phaser";
+
 export default class GameScene extends Phaser.Scene {
-  constructor () {
-    super('Game');
+  constructor() {
+    super("Game");
   }
- 
-  preload () {
+
+  preload() {
     // load images
     this.load.image('logo', '../src/assets/logo.png');
+    this.load.image("sprBg0", "../src/assets/sprBg0.png");
+    this.load.image("sprBg1", "../src/assets/sprBg1.png");
+    this.load.spritesheet("sprExplosion", "../src/assets/sprExplosion.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+    this.load.spritesheet("sprEnemy0", "../src/assets/sprEnemy0.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.image("sprEnemy1", "../src/assets/sprEnemy1.png");
+    this.load.spritesheet("sprEnemy2", "../src/assets/sprEnemy2.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.image("sprLaserEnemy0", "../src/assets/sprLaserEnemy0.png");
+    this.load.image("sprLaserPlayer", "../src/assets/sprLaserPlayer.png");
+    this.load.spritesheet("sprPlayer", "../src/assets/sprPlayer.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
 
+    this.load.audio("sndExplode0", "../src/assets/sndExplode0.wav");
+    this.load.audio("sndExplode1", "../src/assets/sndExplode1.wav");
+    this.load.audio("sndLaser", "../src/assets/sndLaser.wav");
   }
- 
-  create () {
-    this.add.image(400, 300, 'logo');
+
+  create() {
+    this.anims.create({
+      key: "sprEnemy0",
+      frames: this.anims.generateFrameNumbers("sprEnemy0"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "sprEnemy2",
+      frames: this.anims.generateFrameNumbers("sprEnemy2"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "sprExplosion",
+      frames: this.anims.generateFrameNumbers("sprExplosion"),
+      frameRate: 20,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: "sprPlayer",
+      frames: this.anims.generateFrameNumbers("sprPlayer"),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.sfx = {
+      explosions: [
+        this.sound.add("sndExplode0"),
+        this.sound.add("sndExplode1"),
+      ],
+      laser: this.sound.add("sndLaser"),
+    };
+    this.add.image(400, 300, "logo");
   }
-};
+}
